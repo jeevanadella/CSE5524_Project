9cb13090ad0bb322ba1a129e7ed365f1c8960c0a
AdamKH120 <khalil.120@osu.edu>
2025-12-14 16:19:08 -0500

    sbatch file

--- 5524_osc_test-1.sh
+++ 5524_osc_test-1.sh
@@ -4,8 +4,8 @@
 #SBATCH --time=48:00:00
 #SBATCH --cluster=pitzer
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --cpus-per-task=8
+#SBATCH --gpus-per-node=4
+#SBATCH --cpus-per-task=48
 #SBATCH --ntasks-per-node=1
 #SBATCH --output=job_output_%j.txt
 #SBATCH --error=job_error_%j.txt
@@ -13,4 +13,4 @@ source $(conda info --base)/etc/profile.d/conda.sh
 
 conda activate cv_env
 
-python baselines/training/BioClip2-ft-did/train.py
+python trained/training/BioClip2-ft-did/train.py
